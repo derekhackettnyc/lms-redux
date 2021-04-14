6a100b410c4d512a7d1954286eb74951c6b7b3ae
derek hackett <derekhackettnyc@gmail.com>
2021-04-14 11:54:40 +0100

    replaced connect/mapState with useDispatch, useSelector

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux"
 
 import { menuDrawOpened } from '../../actions'
-
 import MainMenu from '../Nav/MainMenu'
 import SearchBar from '../ui/SearchBar'
 
-const Header = ({ nav, isMenuDrawOpened, menuDrawOpened }) => {
+const Header = ({ nav }) => {
+
+    const isMenuDrawOpened = useSelector( state => state.menu.isDrawOpened )
+    const dispatch = useDispatch()
 
     return (
         <header className="header">
@@ -16,7 +19,7 @@ const Header = ({ nav, isMenuDrawOpened, menuDrawOpened }) => {
             </Link>
             <div
 
-                onClick={() => menuDrawOpened(!isMenuDrawOpened)}
+                onClick={() => dispatch(menuDrawOpened(!isMenuDrawOpened))}
                 className={isMenuDrawOpened ? "menu-icon-container change" : "menu-icon-container"}
             >
                 <div className="bar1"></div>
@@ -31,7 +34,7 @@ const Header = ({ nav, isMenuDrawOpened, menuDrawOpened }) => {
 }
 
 
-const mapStateToProps = state => ({ isMenuDrawOpened: state.menu.isDrawOpened })
+// const mapStateToProps = state => ({ isMenuDrawOpened: state.menu.isDrawOpened })
 
-
-export default connect(mapStateToProps, { menuDrawOpened })(Header)
+// export default connect(mapStateToProps, { menuDrawOpened })(Header)
+export default Header
